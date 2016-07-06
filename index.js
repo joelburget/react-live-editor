a0c41d84f390e3b450397be8abf8768125b99097
Joel Burget <joelburget@gmail.com>
2016-07-06 09:10:28 -0400

    Unbreak; bump to 2.0.0.

---
+++ index.js
@@ -0,0 +1,5 @@
+import CodeMirrorEditor from './code-mirror-editor';
+import LiveCompile from './live-compile';
+import LiveEditor from './live-editor';
+
+export default {CodeMirrorEditor, LiveCompile, LiveEditor};
